5c7c5b98898b7d62cbaad7e89b055c9088b8b1d5
Zain ul abdin <zainburfat112@gmail.com>
2022-12-29 18:50:40 +0500

    update

--- src/Providers/CalcServiceProvider.php
+++ src/Providers/CalcServiceProvider.php
@@ -8,16 +8,19 @@ class CalcServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        
+        //
     }
 
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__. '/../../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+        $this->loadTranslationsFrom(__DIR__ . '/../../lang', 'calc');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
+
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'calc');
-        $this->loadTranslationsFrom(__DIR__.'/../../lang', 'calc');
-        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
-        $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
-        
+        $this->publishes([
+            __DIR__ . '/../../resources/views' => resource_path('views/zainburfat/calc'),
+        ]);
     }
 }
